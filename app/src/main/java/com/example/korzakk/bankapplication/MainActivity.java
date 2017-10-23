ae0dcece64126a9c81d8c7d4f8f7377b2b0175d2
JonghyunOh <korzakk@gmail.com>
2017-10-23 12:29:31 -0400

    Linking Buttons to code

--- app/src/main/java/com/example/korzakk/bankapplication/MainActivity.java
+++ app/src/main/java/com/example/korzakk/bankapplication/MainActivity.java
@@ -2,14 +2,39 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button mCreateBtn = (Button)findViewById(R.id.createBtnID);
+        Button mCheckeBtn = (Button)        findViewById(R.id.checkBtnID);
+        Button mAdminBtn = (Button)findViewById(R.id.adminBtnID);
+        Button mExitBtn = (Button)findViewById(R.id.exitBtnID);
 
+        mCheckeBtn.setOnClickListener(this);
+        mCheckeBtn.setOnClickListener(this);
+        mAdminBtn.setOnClickListener(this);
+        mExitBtn.setOnClickListener(this);
     }
-}
+
+     @Override
+     public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.createBtnID:
+                break;
+            case R.id.checkBtnID:
+                break;
+            case R.id.adminBtnID:
+                break;
+            case R.id.exitBtnID:
+                break;
+
+        }
+     }
+ }
